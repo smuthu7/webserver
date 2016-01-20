ad70096429ac09368508d953cbee986b7979b8aa
Muthu Kumar <muthu2988@gmail.com>
2016-01-20 15:39:05 +0000

    Initialize repo

---
+++ server.js
@@ -0,0 +1,24 @@
+var Express = require('express');
+var app = Express();
+var PORT=3000;
+
+var middleware = {
+	requireAuthentication: function(req, res, next){
+		console.log('private route hit!');
+		next();
+	},
+	logger: function(req,res,next){
+		console.log('Request: '+ new Date().toString()+ ' '+ req.method + ' '+ req.originalUrl);
+		next();
+	}
+};
+
+app.use(middleware.logger);
+
+app.get('/about',middleware.requireAuthentication, function(req,res){
+	res.send('This is about Muthu');
+});
+app.use(Express.static(__dirname+'/public'));
+app.listen(PORT, function(){
+	console.log('Express server started on port '+PORT);
+});
